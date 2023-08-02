dc34790bc4f1446bcc98332b9dbf4c7b0ba2a29d
Gerson Ortiz <gersongio0527@gmail.com>
2023-08-02 10:55:58 -0500

    se sube modificaciones

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,29 +1,30 @@
 import React from "react";
+import { FaBars } from "react-icons/fa";
 import "../../app.css";
+
 function Navbar() {
   return (
     <div>
-      <div className="navbar">
-        <div className="navbar__left">
-          <h2>Gerson Ortiz</h2>
-        </div>
-        <div className="navabr__rigth">
-          <ul className="ul">
-            <li>
-              <a href="">inicio</a>
-            </li>
-            <li>
-              <a href="">sobre mi</a>
-            </li>
-            <li>
-              <a href="">proyectos</a>
-            </li>
-            <li>
-              <a href="">contactanos</a>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <nav>
+        <input type="checkbox" id="check"></input>
+        <label htmlFor="check" className="checkbtn">
+          <FaBars />
+        </label>
+        <label className="logo">Gerson Ortiz</label>
+        <ul>
+          <li>
+            <a className="active" href="#">
+              sobre mi
+            </a>
+          </li>
+          <li>
+            <a href="#">proyectos</a>
+          </li>
+          <li>
+            <a href="#">mi cv</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
